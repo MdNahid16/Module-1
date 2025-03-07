9e45bcfee32285a6c53bde8544362696cad30492
Nahid <towhidulislam9292@gmail.com>
2025-03-07 11:36:43 +0600

    First project

---
+++ Module-1/Authentication.php
@@ -0,0 +1,13 @@
+<?php
+define("USERNAME","admin");
+define("PASSWORD","12345");
+
+echo "Enter username: ";
+$inputUsername = readline();
+echo "Enter password: ";
+$inputPassword = readline();
+if ($inputUsername === USERNAME && $inputPassword === PASSWORD){
+    echo "Login successful! \n";
+}else{
+    echo "Invalid username or password. \n";
+}
